419e491f6ff3726536216e08a089b460e5d0e5fe
darielhernandez <13dariel@gmail.com>
2022-04-12 17:54:18 -0400

    Warmup for April 12 2022

--- src/com/darielhernandez/HackerRank/Day00HelloWorld.java
+++ src/com/darielhernandez/HackerRank/Day00HelloWorld.java
@@ -34,3 +34,11 @@ public class Day00HelloWorld {
            bufferedReader.close();
        }
    }
+
+public class warmupChallengeApril12 {
+    public static int triArea(int base, int height) {
+        int area= base*height/2;
+        return area;
+    }
+}
+   
